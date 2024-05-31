3db99626f959d9cf102381aec724ede457df6cfc
Umbrason <45980080+Umbrason@users.noreply.github.com>
2024-06-01 01:27:40 +0200

    Update Radio.gd

--- Levels/Puzzle Objects/Radio/Radio.gd
+++ Levels/Puzzle Objects/Radio/Radio.gd
@@ -4,7 +4,7 @@ signal _channel_state_changed(channel, state)
 
 const channel_count: int = 8
 
-enum Channel {Channel_1 = 1, Channel_2 = 2, Channel_3 = 4, Channel_4 = 8, Channel_5 = 16, Channel_6 = 32, Channel_7 = 64, Channel_8 = 128}
+enum Channel {Channel_1 = 0, Channel_2 = 1, Channel_3 = 2, Channel_4 = 3, Channel_5 = 4, Channel_6 = 5, Channel_7 = 6, Channel_8 = 7}
 
 static var channel_states: Array[int] = []
 
